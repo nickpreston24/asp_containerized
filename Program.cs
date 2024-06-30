4e56f9f8ec10b0df12f24bb8cd53587f34703658
mikepreston17 <michael.n.preston@gmail.com>
2024-06-29 20:02:22 -0500

    added an /html api endpoint to test the possibility of html returns in a standard razor app!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using asp_containerized;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -22,7 +25,19 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
-app.MapGet("/config", 
+app.MapGet("/config",
     () => new { name = "Nick" });
+
+// app.MapGet("/sample-page", () => { return Content("<p>hello there!</p>", "text/html", Encoding.UTF8); }
+// );
+
+app.MapGet("/html", () => Results.Extensions.Html(@$"<!doctype html>
+<html>
+    <head><title>miniHTML</title></head>
+    <body>
+        <h1 class='text-primary border-2 border-red'>Hello World</h1>
+        <p>The time on the server is {DateTime.Now:O}</p>
+    </body>
+</html>"));
 
 app.Run();
